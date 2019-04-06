a30e0c093da9e2308418adb858265beea68d1eab
samfreedgood <sam.freedgood@gmail.com>
2019-04-06 14:58:49 -0400

    basic Dropzone and Home in components/

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import DropZone from "./Dropzone"
+
+class Home extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <DropZone />
+    );
+  }
+}
+ 
+export default Home;
