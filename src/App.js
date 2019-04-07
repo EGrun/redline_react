40251a0fa0378182ca65c7d57bfe421fd58dbe69
samfreedgood <sam.freedgood@gmail.com>
2019-04-07 03:10:37 -0400

    completed style and integration of adminBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from "./components/Home"
+import AdminBar from './components/AdminBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AdminBar />
         <Home />
       </div>
     );
